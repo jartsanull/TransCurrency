623e4b01dc14b84413f72a038010ddc147828ffd
null <151707803+jartsanull@users.noreply.github.com>
2024-05-23 00:19:22 +0300

    Add files via upload

---
+++ TransCurrency/src/main/java/com/example/TransCurrency/web/TransController.java
@@ -0,0 +1,16 @@
+package com.example.TransCurrency.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class TransController {
+
+	@RequestMapping("/main")
+	public String main(Model model) {
+		return "main";
+	  }
+}

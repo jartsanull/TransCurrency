674731b74181784758bf861f612593fbda5f7b5a
null <151707803+jartsanull@users.noreply.github.com>
2024-05-23 00:05:17 +0300

    Add files via upload

---
+++ TransCurrency/src/test/java/com/example/TransCurrency/TransCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TransCurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransCurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

801887c688a51edc1dd2355450d3ba8e250dcab7
jartsanull <langinenj1@gmail.com>
2024-07-30 16:33:44 +0300

    Update TransContRest.java

--- TransCurrency/src/main/java/com/example/TransCurrency/web/TransContRest.java
+++ TransCurrency/src/main/java/com/example/TransCurrency/web/TransContRest.java
@@ -66,7 +66,30 @@ public class TransContRest {
 	}
 
 class ConversionRequest{
-	private String from, to;
+	private String from;
+	private String to;
+	private double amount;
+	
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	
 	
 }
 	

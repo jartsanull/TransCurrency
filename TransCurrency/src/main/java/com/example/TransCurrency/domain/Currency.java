11c19ecea9011059ebaf6eecf02435774ad7c1c3
jartsanull <langinenj1@gmail.com>
2024-07-30 15:45:14 +0300

    lol

--- TransCurrency/src/main/java/com/example/TransCurrency/domain/Currency.java
+++ TransCurrency/src/main/java/com/example/TransCurrency/domain/Currency.java
@@ -6,6 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.List;
+import java.util.Map;
+
+import com.example.TransCurrency.domain.Currency;
+import com.example.TransCurrency.domain.CurrencyRepo;
+import com.example.TransCurrency.service.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
 @Entity
 public class Currency {
 

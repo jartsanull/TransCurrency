2e1c534275d890b5bd3e3fc57ebbd579f07a3d03
jartsanull <langinenj1@gmail.com>
2024-07-30 15:45:28 +0300

    Update TransContRest.java

--- TransCurrency/src/main/java/com/example/TransCurrency/web/TransContRest.java
+++ TransCurrency/src/main/java/com/example/TransCurrency/web/TransContRest.java
@@ -1,5 +1,20 @@
 package com.example.TransCurrency.web;
 
-public class TransContRest {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+import java.util.List;
+import java.util.Map;
+
+import com.example.TransCurrency.domain.Currency;
+import com.example.TransCurrency.domain.CurrencyRepo;
+import com.example.TransCurrency.service.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+public class TransContRest {
+	
 }

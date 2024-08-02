24f301706498ba35f253025e999e15d639218ea1
jartsanull <langinenj1@gmail.com>
2024-08-02 03:59:03 +0300

    Update CurrencyService.java

--- TransCurrency/src/main/java/com/example/TransCurrency/service/CurrencyService.java
+++ TransCurrency/src/main/java/com/example/TransCurrency/service/CurrencyService.java
@@ -1,5 +1,7 @@
 package com.example.TransCurrency.service;
 
+import com.example.TransCurrency.config.AppConfig;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;

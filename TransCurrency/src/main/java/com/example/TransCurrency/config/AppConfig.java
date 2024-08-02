8549aaff41d7f0fe418fd07bd4e4a9b2385d30b7
jartsanull <langinenj1@gmail.com>
2024-08-02 03:55:41 +0300

    Update AppConfig.java

--- TransCurrency/src/main/java/com/example/TransCurrency/config/AppConfig.java
+++ TransCurrency/src/main/java/com/example/TransCurrency/config/AppConfig.java
@@ -1,5 +1,10 @@
 package com.example.TransCurrency.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
 public class AppConfig {
 
 }
